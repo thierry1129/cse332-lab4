0ec6bc03c75a8bfa5f1bac57d04288fbb939489d
Terry Lyu <terrylu@wustl.edu>
2016-12-04 20:31:46 +0000

    added betting phase /// not done

--- FiveCardDraw.h
+++ FiveCardDraw.h
@@ -1,5 +1,7 @@
 #pragma once
 
+
+
 #ifndef FIVECARDDRAW_H
 #define FIVECARDDRAW_H
 
@@ -14,27 +16,31 @@
 
 class Game;
 class FiveCardDraw : public Game {
-    
-    
+
+
 protected:
-    
-    size_t dealer;
-    Deck discard_deck;
-    Deck main_deck;
+
+	size_t dealer;
+	Deck discard_deck;
+	Deck main_deck;
+	unsigned int game_bet;
 private:
-    
-    int cardsPerHand = 5;
-    
+
+	int cardsPerHand = 5;
+	
+
 public:
-    FiveCardDraw();
-    virtual int before_turn(Player &p);
-    virtual int before_round();
-    virtual int turn(Player &p);
-    virtual int after_turn(Player &p);
-    virtual int round();
-    virtual int after_round();
-    static bool handCompare(std::shared_ptr<Player> a, std::shared_ptr<Player> b);
-    
+	FiveCardDraw();
+	virtual int before_turn(Player &p);
+	virtual int before_round();
+	virtual void bet(Player &p);
+
+	virtual int turn(Player &p);
+	virtual int after_turn(Player &p);
+	virtual int round();
+	virtual int after_round();
+	static bool handCompare(std::shared_ptr<Player> a, std::shared_ptr<Player> b);
+
 };
 
 #endif
