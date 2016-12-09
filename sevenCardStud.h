4bd6e48596c82810a7f8a2b156fee641034f1b79
Terry Lyu <terrylu@wustl.edu>
2016-12-09 22:40:50 +0000

    sevenCardStud.h edited online with Bitbucket

--- sevenCardStud.h
+++ sevenCardStud.h
@@ -14,39 +14,42 @@
 
 class Game;
 class SevenCardStud : public Game {
-    
+
 protected:
-    
-    size_t dealer;
-    Deck discard_deck;
-    Deck main_deck;
-    
+
+	size_t dealer;
+	Deck discard_deck;
+	Deck main_deck;
+
 private:
-    
-    int cardsPerHand = 7;
-    unsigned int folded_players = 0;
-    int ante;
-    unsigned int game_bet;
-    
+
+	int cardsPerHand = 7;
+	unsigned int folded_players = 0;
+	int ante;
+	unsigned int game_bet;
+	static bool handCompare(std::shared_ptr<Player> a, std::shared_ptr<Player> b);
 public:
-    SevenCardStud();
-    
-    virtual int before_round();
-    virtual int round();
-    
-    virtual void bet(Player &p);
-    
-    //virtual int before_turn(Player &p);
-    //virtual int turn(Player &p);
-    virtual int after_turn(Player &p);
-    
-    virtual int after_round();
-    static bool handCompare(std::shared_ptr<Player> a, std::shared_ptr<Player> b);
-    
-    virtual int firstTurn(Player &p);
-    virtual int twoThrFourTurn(Player &p);
-    virtual int finalTurn(Player &p);
-    
+	SevenCardStud();
+
+	virtual int before_round();
+	virtual int round();
+
+	virtual void bet(Player &p);
+
+	virtual int before_turn(Player &p);
+	virtual int turn(Player &p);
+	virtual int after_turn(Player &p);
+
+	virtual int after_round();
+	virtual int allFoldWinner();
+
+	virtual int firstTurn(Player &p);
+	virtual int firstTurnCard();
+	virtual int twoThrFourTurn(Player &p);
+	virtual int twoThrFourTurnCard();
+	virtual int finalTurn(Player &p);
+	virtual int finalTurnCard();
+
 };
 
 #endif
