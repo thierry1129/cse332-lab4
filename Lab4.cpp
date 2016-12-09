05b31922a1b45e86a22fdd8c47ff49d131770160
Terry Lyu <terrylu@wustl.edu>
2016-12-09 20:58:27 +0000

    Lab4.cpp edited online with Bitbucket

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,6 +1,3 @@
-//
-//  Lab4.cpp
-
 #include "stdafx.h"
 #include "Cards.h"
 #include "Game.h"
@@ -120,6 +117,68 @@ int main(int argc, char* argv[]) {
 		}
 		else if (gName == game2) {
 			cout << "Now Playing SevenCardStud" << endl;
+			//cout << "test1" << endl;
+			try {
+				//start_game method completed?
+				//cout << "test2" << endl;
+				cardGame.start_game(gameName);
+				//cout << "test2" << endl;
+				gameInst = cardGame.instance();
+			}
+			catch (int e) {
+				return e;
+			}
+			catch (string e) {
+				cout << "error: " << e << endl;
+			}
+			try {
+				cout << "test3" << endl;
+				for (int i = 2; i < argc; ++i) {
+					gameInst->add_player(argv[i]);
+				}
+				//gameInst->add_player(argv[2]);
+				//gameInst->add_player(argv[3]);
+
+				//if (argc > 4) { //creating players when there are more than just two
+				//	for (int i = 4; i < argc; ++i) {
+				//		gameInst->add_player(argv[i]);
+				//	}
+				//}
+			}
+			catch (int e) {
+				cout << "error: " << e << endl;
+			}
+
+			while (gameInst->size() >= 2) {
+				try {
+					cout << "test4" << endl;
+					gameInst->before_round();
+					if (gameInst->allFold != false) {
+						gameInst->allFoldWinner();
+					}
+					else {
+						gameInst->round();
+						gameInst->after_round();
+					}
+
+					if (gameInst->size() < 2) {
+						cout << "not enough players, game over" << endl;
+					}
+				}
+				//catch (...) {
+				catch (string s) {
+					cardGame.stop_game();
+					cout << s << endl;
+					cout << "game failure 2" << endl;
+					break;
+				}
+				catch (const std::exception &exc) {
+					cardGame.stop_game();
+					cout << exc.what() << endl;
+					cout << "game failure" << endl; //another case of a true error exception?
+					break;
+				}
+			}
 			break;
 		}
 		else {
